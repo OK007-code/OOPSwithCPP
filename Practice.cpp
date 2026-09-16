b20521f92e4b71e9c8dde0728cf2da0a5119f57f
Omika <145779808+OK007-code@users.noreply.github.com>
2026-09-16 10:30:40 +0530

    Q5

--- Practice.cpp
+++ Practice.cpp
@@ -124,3 +124,49 @@ int main() {
     d.display(u);
     return 0;
 }
+//Q5: Employee Record using Constant Object
+#include <iostream>
+using namespace std;
+class Employee {
+    int employeeId;
+    string name;
+    float salary;
+public:
+    Employee() {
+        employeeId = 0;
+        name = "Unknown";
+        salary = 0;
+    }
+    Employee(int id, string n, float s) {
+        employeeId = id;
+        name = n;
+        salary = s;
+    }
+    Employee(const Employee &e) {
+        employeeId = e.employeeId;
+        name = e.name;
+        salary = e.salary;
+    }
+    void display() const {
+        cout << "Employee ID: " << employeeId << endl;
+        cout << "Name: " << name << endl;
+        cout << "Salary: " << salary << endl;
+    }
+
+    void updateSalary(float s) {
+        salary = s;
+    }
+};
+int main() {
+    const Employee e1(131, "Omika", 50000);
+    cout << "Const Employee:" << endl;
+    e1.display();
+    // e1.updateSalary(60000);  // Error: cannot modify const object
+    Employee e2(e1);
+    cout << "\nCopied Employee:" << endl;
+    e2.display();
+    e2.updateSalary(60000);
+    cout << "\nAfter Salary Update:" << endl;
+    e2.display();
+    return 0;
+}
