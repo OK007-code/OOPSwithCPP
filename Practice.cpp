393e5e8ebe7a1f2baaa33fc3d390acf284d666c2
Omika <145779808+OK007-code@users.noreply.github.com>
2026-09-16 10:33:13 +0530

    Q6

--- Practice.cpp
+++ Practice.cpp
@@ -170,3 +170,59 @@ int main() {
     e2.display();
     return 0;
 }
+
+//Q6: Student Result Analysis using Friend Class
+#include <iostream>
+using namespace std;
+class ResultAnalyzer;
+class StudentResult {
+    int rollNo;
+    string name;
+    int marks[3];
+    static int totalStudents;
+public:
+    StudentResult(int r, string n, int m1, int m2, int m3) {
+        rollNo = r;
+        name = n;
+        marks[0] = m1;
+        marks[1] = m2;
+        marks[2] = m3;
+        totalStudents++;
+    }
+    friend class ResultAnalyzer;
+    static void showTotalStudents() {
+        cout << "Total Students: " << totalStudents << endl;
+    }
+    ~StudentResult() {
+        cout << "StudentResult object destroyed." << endl;
+    }
+};
+int StudentResult::totalStudents = 0;
+class ResultAnalyzer {
+public:
+    void analyze(StudentResult s) {
+        int total = s.marks[0] + s.marks[1] + s.marks[2];
+        float percentage = total / 3.0;
+
+        cout << "Roll No: " << s.rollNo << endl;
+        cout << "Name: " << s.name << endl;
+        cout << "Total Marks: " << total << endl;
+        cout << "Percentage: " << percentage << "%" << endl;
+
+        if (s.marks[0] >= 40 && s.marks[1] >= 40 && s.marks[2] >= 40)
+            cout << "Result: Pass" << endl;
+        else
+            cout << "Result: Fail" << endl;
+    }
+};
+int main() {
+    StudentResult s1(101, "Omika", 85, 90, 80);
+    StudentResult s2(102, "Riya", 35, 70, 60);
+    ResultAnalyzer r;
+    r.analyze(s1);
+    cout << endl;
+    r.analyze(s2);
+    cout << endl;
+    StudentResult::showTotalStudents();
+    return 0;
+}
